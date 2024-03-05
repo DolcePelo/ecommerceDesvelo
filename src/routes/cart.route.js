a08bf0516d10a99f3cb01472aa7448cb38965585
Alejandro Pelozatto <alejandrojpelozatto@gmail.com>
2024-03-04 23:50:22 -0300

    trabajando con capas

--- src/routes/cart.route.js
+++ src/routes/cart.route.js
@@ -1,141 +1,19 @@
 import { Router } from "express";
-import { CartModel } from "../dao/models/cart.js";
-import { ProductModel } from "../dao/models/product.js";
+import { getCarts, getCartById, createCart,saveProductToCart, deleteProductFromCart, deleteCartById }  from '../controller/cart.controller.js';
 
 const router = Router();
 
-router.get("/", async (req,res) => {
-    try {
-        const result = await CartModel.find();
-        res.json({ message: result })
-    } catch(error) {
-        res.status(500).json({ status: "error", error: "Internal error"})
-    }
-});
+router.get("/", getCarts);
 
-router.get("/:cid", async (req, res) => {
-    try {
-        const { cid } = req.params;
-        const cart = await CartModel.findById(cid);
-        res.json({ message: cart });
-    }catch (error) {
-        res.status(500).json({ status: "error", error: "Internal error"})
-    }
-});
+router.get("/:cid",getCartById);
 
-router.post("/", async (req, res) => {
-    try {
-        const response = await CartModel.create(req.body);
-        res.json(response);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ status: "error", error: "Internal error"});
-    }
-});
+router.post("/", createCart);
 // Agregamos un producto al carrito
-router.put("/:cid/product/:pid", async (req,res) => {
-    const {cid, pid} = req.params;
-
-    const isCartValid = await CartModel.findById(cid);
-    const isProductValid = await ProductModel.findById(pid);
-    let hasChange = false;
-
-    const newProduct = {
-        product: pid,
-        quantity: 1,
-    };
-
-    if(!isCartValid || !isProductValid){
-        return res.status(400).json({
-            status:"error",
-            error:"Cart or product not found",
-        });
-    }
-    
-    const productIndex = isCartValid.products.findIndex(
-        (product) => product.product.equals(pid)
-    );
-
-    if (productIndex === -1) {
-        //Si el producto no existe en la lista lo agrego
-        isCartValid.products.push(newProduct);
-        hasChange=true;
-    } else {
-        isCartValid.products[productIndex].quantity++;
-        hasChange=true;
-    }
-    
-    if(hasChange) {
-        const result = await CartModel.findByIdAndUpdate(cid, {
-            products : isCartValid.products,
-        });
-        res.json({
-            status:'ok',
-            message: isCartValid,
-            result: result,
-        });
-    }
-});
+router.put("/:cid/product/:pid", saveProductToCart);
 
     // Eliminamos un producto del carrito
-    router.delete("/:cid/product/:pid", async (req,res) => {
-        const {cid, pid} = req.params;
-    
-        const isCartValid = await CartModel.findById(cid);
-        const isProductValid = await ProductModel.findById(pid);
-        let hasChange = false;
-    
-        const newProduct = {
-            product: pid,
-            quantity: 1,
-        };
-    
-        if(!isCartValid || !isProductValid){
-            return res.status(400).json({
-                status:"error",
-                error:"Cart or product not found",
-            });
-        }
-    
-        const productIndex = isCartValid.products.findIndex(
-            (product) => product.product.equals(pid)
-        );
-    
-        if (productIndex === -1) {
-            res.status(400).json({
-                status: "error",
-                message: "Product not found"
-            });
-        } else {
-            isCartValid.products[productIndex].quantity--;
-            if(isCartValid.products[productIndex].quantity === 0){
-                isCartValid.products.splice(productIndex, 1);
-            }
-            hasChange=true;
-        }
-        
-        if(hasChange) {
-            const result = await CartModel.findByIdAndUpdate(cid, {
-                products : isCartValid.products,
-            });
-            res.json({
-                status:'ok',
-                message: isCartValid,
-                result: result,
-            });
-        }
-    });
+    router.delete("/:cid/product/:pid", deleteProductFromCart);
 
-
-router.delete("/:cid", async(req,res) => {
-    try {
-    const { cid } = req.params;
-    const result = await CartModel.deleteOne({ _id: cid });
-    res.json({ message: result });
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ status: "error", error: "Internal error"})
-    }
-})
+router.delete("/:cid", deleteCartById)
 
 export default router
--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -1,76 +1,16 @@
 import { Router } from "express";
-import Products from "../dao/dbManager/product.js";
+import {getProducts,getProductById,createProduct,editProduct,deleteProduct} from '../controller/product.controller.js'
 
 const router = Router();
 
-const products = new Products();
+router.get("/", getProducts);
 
-router.get("/", async (req, res) => {
-    try {
-        const response = await products.getAll();
-        res.json({
-            message: "Lista de productos",
-            data: response
-        });
-    } catch (error) {
-        console.log("Error: ", error);
-    }
-});
+router.get("/:id", getProductById);
 
-router.get("/:id", async (req, res) => {
-    const { id } = req.params;
+router.post("/", createProduct);
 
-    try {
-        const response = products.getById(id);
-        res.json(response)
-    } catch (error) {
-        console.log("Error: ", error);
-    }
-});
+router.put("/:id", editProduct);
 
-router.post("/", async (req, res) => {
-    const { name, description, price, category, brand, color, size,code, imageUrl, stock } = req.body;
-    try {
-        const response = await products.saveProduct({
-            name,
-            description,
-            price,
-            category,
-            brand,
-            color,
-            size,
-            code,
-            imageUrl,
-            stock
-        });
-        res.json(response)
-    } catch (error) {
-        console.log('Error al crear el producto', error);
-    }
-});
-
-router.put("/:id", async (req, res) => {
-    const { id } = req.params;
-    const { name, description, price, category, brand, color, size,code, imageUrl, stock } = req.body;
-
-    try {
-        const newProduct = { name, description, price, category, brand, color, size,code, imageUrl, stock };
-
-        const response = await products.updateProduct(id, newProduct);
-        res.json(response);
-    } catch (error) {
-        console.log(`Error actualizando el producto ${id}`, error);
-    };
-});
-
-router.delete("/:id", async (req, res) => {
-    const { id } = req.params;
-    try {
-        const response = await products.deleteProduct(id);
-        res.json(response);
-    } catch (error) {
-        console.log("Error eliminado el producto", error);
-    }
-});
+router.delete("/:id", deleteProduct);
 
 export default router;
